f17963b600b1ff323dfce77e8ce63f068eeb820e
Mike Crowe <mac@mcrowe.com>
2000-04-05 19:18:24 +0000

    Huge improvements so it actually works. There seems to be a problem still with it generating files that are too large though which is odd.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <errno.h>
 #include <limits.h>
+#include <ctype.h>
 
 #define _GNU_SOURCE
 #include <getopt.h>
@@ -82,105 +83,6 @@ int FileDataSource::ReadInto(BYTE *buffer, int bytes_required)
 	return total_bytes;
 }
 
-int ConvertTimeCodeToFrameNumber(MPEGHeader *h, const TimeCode &tc)
-{
-	return ((h->SampleRate() * tc.GetAsHundredths()) / 100)/(h->SamplesPerFrame());
-}
-
-void ProcessFrames(StreamBuffer *input, TimeCode start_tc, TimeCode end_tc)
-{
-	unsigned long header;
-	MPEGHeader h;
-
-	int start_frame = -1;
-	int end_frame = -1;
-	bool found_frame = false;
-	int frame_number = 0;
-
-	try
-	{
-		while (true)
-		{
-			input->EnsureAvailable(4);
-			if (h.Read(input->GetPointer()))
-			{
-				// This is sync, but is the next frame?
-				input->EnsureAvailable(h.FrameLength() + 4);				
-				
-				MPEGHeader h2;
-				if (h2.Read(input->GetPointer() + h.FrameLength()))
-				{
-					// It is, so we can be pretty sure it is an OK frame.
-					if (!found_frame)
-					{
-						start_frame = ConvertTimeCodeToFrameNumber(&h, start_tc);
-						end_frame = ConvertTimeCodeToFrameNumber(&h, end_tc);
-						
-						fprintf(stderr, "Start frame is %d\n", start_frame);
-						fprintf(stderr, "End frame is %d\n", end_frame);
-						if (end_frame == 0)
-							end_frame = INT_MAX;
-						
-						found_frame = true;
-					}
-					
-					// Process frame
-					if (frame_number >= start_frame && frame_number < end_frame)
-					{
-						//fprintf(stderr, "Outputting frame %d (length=%d)\n", frame_number, h.FrameLength());
-						write(1, input->GetPointer(), h.FrameLength());
-					}
-#if 0
-					else
-						fprintf(stderr, "Skipping frame %d\n", frame_number);
-#endif
-					
-					// Now move past it.
-					input->Advance(h.FrameLength());
-					frame_number++;
-					continue;
-				}
-			}
-			// OK, if we got here then it wasn't valid sync.
-			input->Advance(1);
-		}
-	}
-	catch (InsufficientDataException &)
-	{
-		// We've run out of data - that's fine
-	}
-	catch (FileException &e)
-	{
-		std::cerr << "File error: " << e.Description() << std::endl;
-	}
-}
-
-bool ProcessFile(const TimeCode &begin_tc, const TimeCode &end_tc, DataSource *ds)
-{
-	fprintf(stderr, " Start time: %d:%02d:%02d.%02d\n",
-			begin_tc.GetHours(), begin_tc.GetMinutes(), begin_tc.GetSeconds(), begin_tc.GetHundredths());
-	if (end_tc.GetAsHundredths())
-	{
-		fprintf(stderr, " End time: %d:%02d:%02d.%02d\n",
-				end_tc.GetHours(), end_tc.GetMinutes(), end_tc.GetSeconds(), end_tc.GetHundredths());
-	}
-
-	try
-	{
-		StreamBuffer input(131072);	
-		input.SetSource(ds);
-
-		ProcessFrames(&input, begin_tc, end_tc);
-		
-		return true;
-	}
-	catch (FileException &e)
-	{
-		std::cerr << "File error: " << e.Description() << std::endl;
-		return false;
-	}
-}
-
 class MP3Processor
 {
 public:
@@ -209,8 +111,15 @@ private:
 	TimeCode begin_tc;
 	TimeCode end_tc;
 	int files;
+	bool keep_id3;
 
 	static void ParseTimeCode(TimeCode *tc, const std::string &t);
+	static int ConvertTimeCodeToFrameNumber(MPEGHeader *h, const TimeCode &tc);
+	static bool IsID3Header(const BYTE *p);
+	
+	bool ProcessFile(const TimeCode &begin_tc, const TimeCode &end_tc, DataSource *ds);
+	void ProcessFrames(StreamBuffer *input, TimeCode start_tc, TimeCode end_tc);
+	void HandleID3Tag(StreamBuffer *input);
 
 public:
 	MP3Processor() : begin_tc(0), end_tc(0), files(0) {};
@@ -218,8 +127,155 @@ public:
 	void HandleFile(const std::string &);
 	void HandleBeginTimeCode(const std::string &);
 	void HandleEndTimeCode(const std::string &);
+	void SetKeepID3(bool b = true) { keep_id3 = b; }
 	void HandleEnd();
 };
+
+int MP3Processor::ConvertTimeCodeToFrameNumber(MPEGHeader *h, const TimeCode &tc)
+{
+	return ((h->SampleRate() * tc.GetAsHundredths()) / 100)/(h->SamplesPerFrame());
+}
+
+bool MP3Processor::IsID3Header(const BYTE *p)
+{
+	return (p[0] == 'T') && (p[1] == 'A') && (p[2] == 'G');
+}
+
+void MP3Processor::ProcessFrames(StreamBuffer *input, TimeCode start_tc, TimeCode end_tc)
+{
+	unsigned long header;
+	MPEGHeader h;
+
+	int start_frame = -1;
+	int end_frame = -1;
+	bool found_frame = false;
+	int frame_number = 0;
+	bool found_sync = 0;
+
+	try
+	{
+		while (true)
+		{
+			input->EnsureAvailable(4);
+			if (h.Read(input->GetPointer()))
+			{
+				if (!found_sync)
+				{
+					// This is sync, but is the next frame?
+					input->EnsureAvailable(h.FrameLength() + 4);				
+				
+					MPEGHeader h2;
+					if (!h2.Read(input->GetPointer() + h.FrameLength()))
+					{
+						// Nope - we haven't found sync.
+						input->Advance(1);
+						continue;
+					}
+					found_sync = true;
+				}
+
+				input->EnsureAvailable(h.FrameLength());
+				
+				// It is, so we can be pretty sure it is an OK frame.
+				if (!found_frame)
+				{
+					start_frame = ConvertTimeCodeToFrameNumber(&h, start_tc);
+					end_frame = ConvertTimeCodeToFrameNumber(&h, end_tc);
+					
+					fprintf(stderr, "Start frame is %d\n", start_frame);
+					fprintf(stderr, "End frame is %d\n", end_frame);
+					if (end_frame == 0)
+						end_frame = INT_MAX;
+					
+					found_frame = true;
+				}
+				
+				// Process frame
+				if (frame_number >= start_frame && frame_number < end_frame)
+				{
+					//fprintf(stderr, "Outputting frame %d (length=%d)\n", frame_number, h.FrameLength());
+					write(1, input->GetPointer(), h.FrameLength());
+				}
+#if 0
+				else
+					fprintf(stderr, "Skipping frame %d\n", frame_number);
+#endif
+				
+				// Now move past it.
+				input->Advance(h.FrameLength());
+				frame_number++;
+				continue;
+			}
+			// OK, if we got here then it wasn't valid sync.
+			BYTE b = *(input->GetPointer());
+			fprintf(stderr, "Lost sync on character '%c' (%d)\n", isprint(b) ? b : '.', b);
+
+			input->Advance(1);
+			found_sync = false;
+		}
+	}
+	catch (InsufficientDataException &)
+	{
+		// We've run out of data - that's fine, just suck up the remaining bytes
+		while (input->GetAvailable())
+			input->Advance(1);
+	}
+	catch (FileException &e)
+	{
+		std::cerr << "File error: " << e.Description() << std::endl;
+	}
+}
+
+void MP3Processor::HandleID3Tag(StreamBuffer *input)
+{
+	try
+	{
+		// We're at the end of the file - go 128 bytes back.
+		input->Rewind(128);
+		BYTE ch = *(input->GetPointer());
+		fprintf(stderr, "Byte at -128 is %c (0x%x)\n", isprint(ch) ? ch : '.', ch);
+		if (IsID3Header(input->GetPointer()))
+		{
+			fprintf(stderr, "Found an ID3 tag.\n");
+			// We've got an ID3 header. Just output the block.
+			write(1, input->GetPointer(), 128);
+		}
+	}
+	catch(InsufficientDataException &)
+	{
+		std::cerr << "Bad thing happened - less than 128 bytes there for ID3 checking." << std::endl;
+	}
+}
+				
+		
+
+bool MP3Processor::ProcessFile(const TimeCode &begin_tc, const TimeCode &end_tc, DataSource *ds)
+{
+	fprintf(stderr, " Start time: %d:%02d:%02d.%02d\n",
+			begin_tc.GetHours(), begin_tc.GetMinutes(), begin_tc.GetSeconds(), begin_tc.GetHundredths());
+	if (end_tc.GetAsHundredths())
+	{
+		fprintf(stderr, " End time: %d:%02d:%02d.%02d\n",
+				end_tc.GetHours(), end_tc.GetMinutes(), end_tc.GetSeconds(), end_tc.GetHundredths());
+	}
+
+	try
+	{
+		StreamBuffer input(131072, 128);	
+		input.SetSource(ds);
+
+		ProcessFrames(&input, begin_tc, end_tc);
+		if (keep_id3)
+			HandleID3Tag(&input);
+		
+		return true;
+	}
+	catch (FileException &e)
+	{
+		std::cerr << "File error: " << e.Description() << std::endl;
+		return false;
+	}
+}
 
 void MP3Processor::InvalidTimeCodeException::Report()
 {
@@ -291,10 +347,11 @@ int main(int ac, char *av[])
 	{
 		{ "begin", 1, NULL, 'b' },
 		{ "end", 1, NULL, 'e' },
+		{ "id3", 1, NULL, 'i' },
 		{ 0, 0, 0, 0 }
 	};
 
-	const char *short_options = "-b:e:";
+	const char *short_options = "-b:e:i";
 
 	MP3Processor processor;
 
@@ -315,6 +372,9 @@ int main(int ac, char *av[])
 				break;
 			case 'e':
 				processor.HandleEndTimeCode(optarg);
+				break;
+			case 'i':
+				processor.SetKeepID3(true);
 				break;
 			case '?':
 				exit(1);
