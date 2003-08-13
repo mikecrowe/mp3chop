f9d1087a2cf3fad9652c1238c7964755da7b44ac
Mike Crowe <mac@mcrowe.com>
2003-08-13 12:26:26 +0000

    Windows can't build its own one of these.

---
+++ config_win32.h
@@ -0,0 +1,21 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader 2.13.  */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you have the getopt function.  */
+#define HAVE_GETOPT 1
+
+/* Define if you have the getopt_long function.  */
+#define HAVE_GETOPT_LONG 1
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Name of package */
+#define PACKAGE "mp3chop"
+
+/* Version number of package */
+#define VERSION "0.02"
+
