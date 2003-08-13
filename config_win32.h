5b80d6b7f83bd517fdf17b8882a18ba6b7b9e7d5
Mike Crowe <mac@mcrowe.com>
2003-08-13 12:37:44 +0000

    Lots more HAVEs.

--- config_win32.h
+++ config_win32.h
@@ -1,6 +1,12 @@
 /* config.h.  Generated automatically by configure.  */
 /* config.h.in.  Generated automatically from configure.in by autoheader 2.13.  */
 
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define as __inline if that's what the C compiler calls it.  */
+/* #undef inline */
+
 /* Define if you have the ANSI C header files.  */
 #define STDC_HEADERS 1
 
@@ -10,8 +16,20 @@
 /* Define if you have the getopt_long function.  */
 #define HAVE_GETOPT_LONG 1
 
+/* Define if you have the memmove function.  */
+#define HAVE_MEMMOVE 1
+
 /* Define if you have the strerror function.  */
 #define HAVE_STRERROR 1
+
+/* Define if you have the <fcntl.h> header file.  */
+#define HAVE_FCNTL_H 1
+
+/* Define if you have the <string.h> header file.  */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
 
 /* Name of package */
 #define PACKAGE "mp3chop"
